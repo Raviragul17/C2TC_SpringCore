3e1d5fd8af8eec060070320fadff304cda090846
Raviragul17 <your_github_email@example.com>
2025-11-05 10:47:47 +0530

    ,

---
+++ dependencyInjection/src/main/java/com/tnsif/dependencyInjection/App.java
@@ -0,0 +1,13 @@
+package com.tnsif.dependencyInjection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
